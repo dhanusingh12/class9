2149247c5834c26c7ab308e380fda14f668760d1
dhanusingh12 <104127835+dhanusingh12@users.noreply.github.com>
2022-04-21 12:52:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(200,200,20,30)
+}
+
+function draw() 
+{
+  background(30);
+drawSprites()
+if (keyIsDown(RIGHT_ARROW)){
+  box.position.x+=5
+}
+if (keyIsDown(LEFT_ARROW)){
+  box.position.x-=5
+}
+}
+
+
+
+
